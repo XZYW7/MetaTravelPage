127390bc00999925ccdbebd595482b2c824a22eb
XZYW <928144597@qq.com>
2026-05-03 17:38:25 +0700

    chore: 添加本地服务器启动脚本

---
+++ start-server.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Start local HTTP server for travel-page
+echo Starting local server...
+echo Open http://localhost:8000 in your browser
+echo Press Ctrl+C to stop the server
+python -m http.server 8000
